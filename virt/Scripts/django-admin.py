e082aa99fe5401013f463d9851641c94958f1ebc
S4601 <alexandercholakov1@gmail.com>
2023-02-28 12:35:01 +0200

    first commit

---
+++ virt/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\Documents\AlexanderCholakov 10b 2\PythonProject\flashcards\virt\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
